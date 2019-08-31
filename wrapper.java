628e083b5cfe0736e39b8bfeed4ed480d1edda19
Mastan <mastanvali9642@gmail.com>
2019-08-31 18:09:10 +0530

    first commit

---
+++ wrapper.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class WrapperEx {
+	public static void main(String[] args) {
+		
+		Integer a=130;
+		Integer b=130;
+		if(a==b) {
+			System.out.println("equal");
+		}
+		
+	}
+
+}
